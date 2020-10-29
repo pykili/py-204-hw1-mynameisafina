6b5b82db846f64454662bc60970899ece6900e3c
mynameisafina <71991048+mynameisafina@users.noreply.github.com>
2020-10-29 22:09:06 +0300

    Update solution_3_2.py

--- task_3/solution_3_2.py
+++ task_3/solution_3_2.py
@@ -1,6 +1,6 @@
-# your code here
+alphabet=""
 for smth in 'a'*10:
-    # your code here
-    user_input = input()
-# your code here
+    user_input = input("введите буковки я на коленях УМОЛЯЮ:")
+    for letter in user_input:
+    	if letter not in alphabet: alphabet+=letter 
 print(alphabet)
