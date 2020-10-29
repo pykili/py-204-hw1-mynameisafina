7718a9307be75bf47dcb1c8a306cb25c89503531
mynameisafina <71991048+mynameisafina@users.noreply.github.com>
2020-10-29 23:32:30 +0300

    Update solution_2.py

--- task_2/solution_2.py
+++ task_2/solution_2.py
@@ -1,4 +1,11 @@
-# your code here
-user_input = input()
-# your code here
+inp = str(input("введите последовательность букв:"))
+
+most_frequent_character = ''
+count = 0
+
+for char in inp:
+	if inp.count(char) > count:
+		most_frequent_character = char
+		count = inp.count(char)
+
 print(most_frequent_character)
